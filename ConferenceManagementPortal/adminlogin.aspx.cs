6d61eca44671a1eeee21e4154eea7597fe27ab19
Neeraj Anand <56186425+neeraj-2@users.noreply.github.com>
2022-02-10 20:52:58 +0530

    database created and connection string added

--- ConferenceManagementPortal/adminlogin.aspx.cs
+++ ConferenceManagementPortal/adminlogin.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,6 +12,7 @@ namespace ConferenceManagementPortal
 {
     public partial class WebForm6 : System.Web.UI.Page
     {
+        string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -16,7 +20,42 @@ namespace ConferenceManagementPortal
 
         protected void Button1_Click(object sender, EventArgs e)
         {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                string query = "select * from admin_login_tbl where username = '" + TextBox1.Text.Trim() + "' and password='" + TextBox2.Text.Trim() + "';";
 
+
+               
+                SqlCommand cmd = new SqlCommand(query, con);
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        Response.Write("<script>alert('Login Succesful')</script>");
+                        Session["username"] = dr.GetValue(0).ToString();
+                        Session["fullname"] = dr.GetValue(2).ToString();
+                        Session["role"] = "admin";
+
+                    }
+                    Response.Redirect("homepage.aspx");
+                }
+                else
+                {
+                    Response.Write("<script>alert('Invalid Credentials')</script>");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
         }
     }
+    
 }
