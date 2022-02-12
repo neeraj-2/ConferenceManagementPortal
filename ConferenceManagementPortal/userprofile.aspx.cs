c990ed34c217363150c5c860ae919fe1bd2a554c
Neeraj Anand <56186425+neeraj-2@users.noreply.github.com>
2022-02-13 01:51:51 +0530

    edit paper functionality added

--- ConferenceManagementPortal/userprofile.aspx.cs
+++ ConferenceManagementPortal/userprofile.aspx.cs
@@ -164,5 +164,42 @@ namespace ConferenceManagementPortal
 
             }
         }
+
+        
+        protected void DownloadFile(object sender, EventArgs e)
+        {
+        int book_id = int.Parse((sender as LinkButton).CommandArgument);
+        byte[] bytes;
+        string fileName, contentType;
+        string constr = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+        using (SqlConnection con = new SqlConnection(constr))
+            {
+            using (SqlCommand cmd = new SqlCommand())
+                {
+                cmd.CommandText = "select Name, Data, ContentType from uploaded_files_tbl where book_id=@book_id";
+                cmd.Parameters.AddWithValue("@book_id", book_id);
+                cmd.Connection = con;
+                con.Open();
+                using (SqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                    sdr.Read();
+                    bytes = (byte[])sdr["Data"];
+                    contentType = sdr["ContentType"].ToString();
+                    fileName = sdr["Name"].ToString();
+                    }
+                con.Close();
+                }
+            }
+        Response.Clear();
+        Response.Buffer = true;
+        Response.Charset = "";
+        Response.Cache.SetCacheability(HttpCacheability.NoCache);
+        Response.ContentType = contentType;
+        Response.AppendHeader("Content-Disposition", "attachment; filename=" + fileName);
+        Response.BinaryWrite(bytes);
+        Response.Flush(); 
+        Response.End();
+            }
+        
     }
 }
