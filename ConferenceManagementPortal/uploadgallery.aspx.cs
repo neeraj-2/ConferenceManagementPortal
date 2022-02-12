d43e0889b49091b43924ba4e48c9684fd4d0e484
Neeraj Anand <56186425+neeraj-2@users.noreply.github.com>
2022-02-12 20:06:44 +0530

    download functionality added

--- ConferenceManagementPortal/uploadgallery.aspx.cs
+++ ConferenceManagementPortal/uploadgallery.aspx.cs
@@ -43,6 +43,23 @@ namespace ConferenceManagementPortal
             }
         }
 
+        private void BindGrid()
+    {
+        string constr = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+        using (SqlConnection con = new SqlConnection(constr))
+        {
+            using (SqlCommand cmd = new SqlCommand())
+            {
+                cmd.CommandText = "select member_id, title, category,description,book_id, Name from uploaded_files_tbl";
+                cmd.Connection = con;
+                con.Open();
+                GridView1.DataSource = cmd.ExecuteReader();
+                GridView1.DataBind();
+                con.Close();
+            }
+        }
+    }
+
         protected void Button1_Click(object sender, EventArgs e)
         {
             string filename = Path.GetFileName(FileUpload1.PostedFile.FileName);
@@ -76,9 +93,54 @@ namespace ConferenceManagementPortal
                     }
                 }
             }
+            clearForm();
             Response.Write("<script>alert('Uploaded Succesfully. Thanks')</script>");
-            GridView1.DataBind();
+           GridView1.DataBind();
+           
             
         }
+
+        private void clearForm()
+        {
+            TextBox2.Text = "";
+            TextBox3.Text = "";
+            TextBox6.Text = "";
+            TextBox4.Text = "";
+        }
+
+        protected void DownloadFile(object sender, EventArgs e)
+    {
+        int book_id = int.Parse((sender as LinkButton).CommandArgument);
+        byte[] bytes;
+        string fileName, contentType;
+        string constr = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+        using (SqlConnection con = new SqlConnection(constr))
+        {
+            using (SqlCommand cmd = new SqlCommand())
+            {
+                cmd.CommandText = "select Name, Data, ContentType from uploaded_files_tbl where book_id=@book_id";
+                cmd.Parameters.AddWithValue("@book_id", book_id);
+                cmd.Connection = con;
+                con.Open();
+                using (SqlDataReader sdr = cmd.ExecuteReader())
+                {
+                    sdr.Read();
+                    bytes = (byte[])sdr["Data"];
+                    contentType = sdr["ContentType"].ToString();
+                    fileName = sdr["Name"].ToString();
+                }
+                con.Close();
+            }
+        }
+        Response.Clear();
+        Response.Buffer = true;
+        Response.Charset = "";
+        Response.Cache.SetCacheability(HttpCacheability.NoCache);
+        Response.ContentType = contentType;
+        Response.AppendHeader("Content-Disposition", "attachment; filename=" + fileName);
+        Response.BinaryWrite(bytes);
+        Response.Flush(); 
+        Response.End();
+    }
     }
 }
